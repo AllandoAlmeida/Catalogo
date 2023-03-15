1bbb0add1cf18be2f5117e23bc6ec41e06a96478
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-03-15 11:14:54 -0300

    Desenvolvimento atividade Catálogo Geek

--- script.js
+++ script.js
@@ -36,3 +36,70 @@ let itens = [
         type: 'Action Figures'
     }
 ];
+
+
+
+let divListFrmes = document.querySelector(".paintings");
+let divActionFigures = document.querySelector(".actionFigures");
+
+let sectionListFrmes = document.createElement("section");
+let sectionlistFigures = document.createElement("section");
+
+let titleFrmes = document.createElement('span');
+let titleFigures = document.createElement('span');
+
+let listFrames = document.createElement("ul");
+let listFigures = document.createElement("ul");
+
+divListFrmes.appendChild(titleFrmes);
+divActionFigures.appendChild(titleFigures);
+
+divListFrmes.appendChild(sectionListFrmes);
+divActionFigures.appendChild(sectionlistFigures);
+
+sectionListFrmes.appendChild(listFrames);
+sectionlistFigures.appendChild(listFigures);
+
+titleFrmes.innerText = 'Paintings';
+titleFigures.innerText = 'Action Figures'
+
+
+function createProductCard(product) {
+    let productCard = document.createElement("li");
+    let imageCard = document.createElement("img");
+    let nameCard = document.createElement("h2");
+    let priceCard = document.createElement("p");
+
+    imageCard.src = product.image;
+    nameCard.innerText = product.name;
+    priceCard.innerText = product.price;
+    
+    productCard.id = product.name;
+    
+    productCard.className = "product-card";
+    imageCard.className = "image-card"
+
+    productCard.appendChild(imageCard);
+    productCard.appendChild(nameCard);
+    productCard.appendChild(priceCard);
+
+    return productCard;
+
+}
+
+function listProduct (array, type, loja){
+
+    for (let i = 0; i < array.length; i++){
+        if (array[i].type == type) {
+            let result = createProductCard(array[i])
+        loja.appendChild(result);
+        }
+    }
+}
+
+listProduct(itens, 'Painting', listFrames );
+listProduct(itens, 'Action Figures', listFigures );
+
+
+
+
